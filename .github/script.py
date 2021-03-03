a5ddc993a9c97851eb133b9eaed40a5875151de6
Mohammad Shahdad <MohammadShahdad@users.noreply.github.com>
2021-03-03 14:54:24 +0530

    Update script.py

--- .github/script.py
+++ .github/script.py
@@ -9,7 +9,7 @@ telegram_api = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"
 
 telegram_token = os.environ["TOKEN"]
 
-s_code = "TQB 2001E"
+s_code = "CCUB 4621"
 
 auth_data = {
     "mat": os.environ["MAT"],
@@ -30,5 +30,7 @@ for row in rows:
     cols = row.select("td")
     out[cols[1].text] = cols[2].text
 
-msg = "\n".join([f"Section: {k}, Vacancy: {v}" for k,v in out.items()])
+msg1 = s_code
+msg2 = "\n".join([f"Section: {k}, Vacancy: {v}" for k,v in out.items()])
+msg = msg1 + msg2
 requests.get(telegram_api%(telegram_token,chat_id,msg))
